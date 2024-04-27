fb5752772c42b483cdec9bb214c472efa992005d
gihan <gamageg-cs19007@stu.kln.ac.lk>
2024-04-27 19:02:48 +0530

    spring project downloaded from spring.io

---
+++ src/test/java/com/giga/cleaning/service/CleaningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giga.cleaning.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
